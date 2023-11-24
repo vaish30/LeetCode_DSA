3f1de21a08e8c42759f397ae4b529dfc490a3148
Vaish <30567157+vaish30@users.noreply.github.com>
2023-11-23 22:10:19 -0500

    Time: 468 ms (80.90%) | Memory: 31.9 MB (25.63%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -2,24 +2,24 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
         #using hash set - which contains no duplicates 
-        # arr = set()
-        # for i in nums:
-        #     if i in arr:
-        #         return True 
-        #     arr.add(i)
-        # return False
+        arr = set()
+        for i in nums:
+            if i in arr:
+                return True 
+            arr.add(i)
+        return False
         
         
         
         #using hash map
-        arr = {}
-        for i in nums:
-            if i in arr :
-                return True
-            arr[i] = arr.get(i,0) + 1
-            print(i, arr[i])
+        # arr = {}
+        # for i in nums:
+        #     if i in arr :
+        #         return True
+        #     arr[i] = arr.get(i,0) + 1
+        #     print(i, arr[i])
             
-        return False
+        # return False
 
 
 
