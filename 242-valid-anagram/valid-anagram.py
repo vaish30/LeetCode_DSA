64b023aac289848c5b3b1ca915aeb7e4e99799e6
Vaish <30567157+vaish30@users.noreply.github.com>
2023-11-25 00:29:12 -0500

    Time: 54 ms (58.41%) | Memory: 17 MB (30.76%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,22 @@
+#get count for each letter in s and t 
+# compare it with 
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = dict()
+        t_dict = dict()
+
+        for i in s:
+            s_dict[i] = s_dict.get(i,0) + 1
+
+        for i in t:
+            t_dict[i] = t_dict.get(i,0) + 1
+
+        print(s_dict, t_dict)
+
+        return s_dict == t_dict
+
+
+
+
