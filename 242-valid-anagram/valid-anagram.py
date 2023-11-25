8ed4dd7c9714b04c7b16c522dd542adddc0c80b4
Vaish <30567157+vaish30@users.noreply.github.com>
2023-11-25 00:37:26 -0500

    Time: 56 ms (46.83%) | Memory: 16.8 MB (40.87%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,7 +5,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_dict = dict()
-        t_dict = dict()
+        # t_dict = dict()
 
         for i in s:
             s_dict[i] = s_dict.get(i,0) + 1
