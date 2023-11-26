0b49d5fb4e49ba61bb15f51b1938f8a3ea62157b
Vaish <30567157+vaish30@users.noreply.github.com>
2023-11-26 17:52:20 -0500

    Time: 521 ms (68.97%) | Memory: 30.5 MB (10.39%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -1,5 +1,5 @@
 class Solution:
-    def getWinner(self, arr: List[int], k: int) -> int:
+    def getWinner_mine(self, arr: List[int], k: int) -> int:
         if k == 1:
             return max(arr[0],arr[1])
         if k >=len(arr):
@@ -18,6 +18,34 @@ class Solution:
                 return winner
 
         return winner
+
+    
+    #implementing deque where we need quicker append and pop from the queue both the ends - functions used are append and popleft()
+
+    def getWinner(self, arr: List[int], k: int) -> int:
+        q = deque(arr)
+        one = q.popleft()
+
+        x=k
+
+        maximum = max(arr)
+
+        if k>= len(arr):
+            return maximum
+
+        while k:
+            two = q.popleft()
+            if one > two:
+                k -=1
+                q.append(two)
+            else:
+                k=x
+                k-=1
+                q.append(one)
+                one = two
+
+        return one
+
 
                 
 
