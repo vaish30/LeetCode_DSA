b64c1a476ec548f269843a5d189ddf7b3dda7589
Vaish <30567157+vaish30@users.noreply.github.com>
2023-12-11 14:58:24 -0500

    Time: 36 ms (78.95%) | Memory: 16.3 MB (27.52%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -4,7 +4,7 @@ class Solution:
         if str1 + str2 != str2 +str1:
             return ""
 
-        return str1[:gcd(len(str1),len(str2))]
+        return str1[:gcd(len(str1),len(str2))] #finding the strings by slicing from beginning of the string omitting 0 till gcd
         
         
         
