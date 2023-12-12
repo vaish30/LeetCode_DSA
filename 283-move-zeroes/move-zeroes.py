32239dfad393b25793b314dcbf533e8622e1a626
Vaish <30567157+vaish30@users.noreply.github.com>
2023-12-12 14:48:39 -0500

    Time: 143 ms (72.13%) | Memory: 18 MB (29.24%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,9 +4,12 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums.remove(nums[i])
-                nums.append(0) 
-            else:
-                i+=1       
+        # Initialize two pointers
+        left, right = 0, 0
+
+        # Move non-zero elements to the left
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
