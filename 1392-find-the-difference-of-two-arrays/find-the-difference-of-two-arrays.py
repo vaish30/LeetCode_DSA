5290ad436ef42264bf9ebdee72744723b85e9716
Vaish <30567157+vaish30@users.noreply.github.com>
2023-12-19 12:17:10 -0500

    Time: 176 ms (28.60%) | Memory: 16.7 MB (74.77%) - LeetSync

---
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        hashtable = {}
+        first_true_second_false = []
+        second_true_first_false = []
+        for num in nums1:
+            if num not in hashtable:
+                hashtable[num] = [True,False]
+        for num in nums2:
+            if num in hashtable:
+                hashtable[num][1] = True
+            else:
+                hashtable[num] = [False,True]
+        for key, value in hashtable.items():
+            if value[0] and not value[1]:
+                first_true_second_false.append(key)
+            elif not value[0] and value[1]:
+                second_true_first_false.append(key)
+        return [first_true_second_false,second_true_first_false]
+        
+        
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        
