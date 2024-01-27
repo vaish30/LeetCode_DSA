1622c05d327ad661f0abb59adb4af7bf1471778f
Vaish <30567157+vaish30@users.noreply.github.com>
2024-01-27 16:28:26 -0500

    Time: 38 ms (63.88%) | Memory: 16.4 MB (81.82%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,17 +1,8 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        #basecase
-        if str1 + str2 != str2 +str1:
+        if str1 + str2 == str2 + str1:
+            return str1[:gcd(len(str1),len(str2))]
+        else:
             return ""
 
-        return str1[:gcd(len(str1),len(str2))] #finding the strings by slicing from beginning of the string omitting 0 till gcd
         
-        
-        
-        
-        
-        
-        
-        
-        
- 
