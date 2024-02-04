b2b49b0fbe56df9c6d7e5f72a6a5ba382c014747
Vaish <30567157+vaish30@users.noreply.github.com>
2024-02-04 14:18:44 -0500

    Time: 54 ms (58.10%) | Memory: 17.5 MB (83.58%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,21 +1,27 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        string = list(s)  # Convert the string to a list of characters
-        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        st = s.lower()
+        st = list(s)
+        dictionary = ['a','e','i','o','u','A','E','I','O','U']
+        
 
-        first, second = 0, len(string) - 1
+        x, y = 0, len(st)-1
 
-        while first < second:
-            if string[first].lower() in vowels and string[second].lower() in vowels:
-                # Swap characters
-                string[first], string[second] = string[second], string[first]
-                first += 1
-                second -= 1
+        while x<y:
+            if st[x] in dictionary and st[y] in dictionary:
+                st[x],st[y] = st[y],st[x]
+                x+=1
+                y-=1
+            
 
-            # Move the pointers if the characters are not vowels
-            if string[first].lower() not in vowels:
-                first += 1
-            if string[second].lower() not in vowels:
-                second -= 1
+            if st[x] not in dictionary:
+                x+=1
+            
+            if st[y] not in dictionary:
+                y-=1
 
-        return "".join(string)  # Convert the list of characters back to a string
+            # print(s[x],s[y])            
+
+        return "".join(st)
+            
+        
