9a549579e4d6b42b6db7d8258b9aaeed4b1b6460
Vaish <30567157+vaish30@users.noreply.github.com>
2024-11-13 00:12:03 -0500

    Time: 54 ms (5.03%) | Memory: 25.7 MB (97.95%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,6 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+
+    def eff_containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
 
         for n in nums:
@@ -8,5 +9,21 @@ class Solution:
             hashset.add(n)     
         
         return False
-        
-        
+
+    def bad_containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]== nums[j]:
+                    return True
+
+        return False
+
+    
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        new_nums = nums.sort()
+
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+
+        return False
