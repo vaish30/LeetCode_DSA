5dcc31846543fa27c9bd5f4679fe934f0fa2b0d3
Vaish <30567157+vaish30@users.noreply.github.com>
2024-11-13 02:13:46 -0500

    Time: 1719 ms (21.16%) | Memory: 17.3 MB (78.12%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,29 +1,11 @@
-#brute force method 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
+
+        n_list = sorted(nums)
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
+            for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    output = i,j
+                    return i,j
 
-        return output
-                    
-
-# #hash table method 
-#     def twoSum(self,num: List[int], target: int) -> List[int]:
-#         table = {}
-
-#         # for i in range(len(num)):
-#         #     complement = target - num[i]
-
-#         #     if complement in table:
-#         #         return [table[complement],i]
-            
-#         #     table[num[i]] = i
-#         #     print(num[i],i)
-
-#         # return []
-
-
-        
+        return False       
