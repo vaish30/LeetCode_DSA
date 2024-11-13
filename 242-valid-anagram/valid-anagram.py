35308e9107257a4661ca4fd2eddd231a5ede2cc3
Vaish <30567157+vaish30@users.noreply.github.com>
2024-11-13 02:03:50 -0500

    Time: 12 ms (59.57%) | Memory: 16.9 MB (70.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,31 +1,23 @@
-#get count for each letter in s and t 
-# compare it with 
-
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = dict()
-        t_dict = dict()
+        if len(s) != len(t):
+            return False
 
-        # for i in t:
-        #     s_dict[i] = s_dict.get(i,0) - 1
+        h_s, h_t = {}, {} #multiple assignment
 
-        # for i in s_dict:
-        #     if s_dict[i] != 0:
-        #         return False
-        # return True 
+        for i in range( len(s)):
+            h_s[s[i]] = 1 + h_s.get(s[i],0)
+            h_t[t[i]] = 1 + h_t.get(t[i],0)
 
+        print(h_s)
+        print(h_t)
 
-        for i in s:
-            s_dict[i] = s_dict.get(i,0) + 1
-
-        for i in t:
-            t_dict[i] = t_dict.get(i,0) + 1
-
-        print(s_dict, t_dict)
-
-        return s_dict == t_dict
+        for c in h_s:
+            if h_s[c] != h_t.get(c,0):
+                return False
+                
+        return h_s == h_t
 
 
 
-
+        
