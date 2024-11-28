73e98eacfbc83cb64ddba50ba0b39eb83bf3f533
Vaish <30567157+vaish30@users.noreply.github.com>
2024-11-28 02:09:32 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (6.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,42 +1,12 @@
 class Solution:
-    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
-        output = []
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers)-1
 
-        left, right = 0, len(numbers)-1
-
-        while left<right:
-            # mid = left + (right - left) // 2
-
-            sum = numbers[right]+numbers[left]  
-
-            if sum == target:
-                output.append(left+1)
-                output.append(right+1)
-                return output
-            
-            
-            elif sum < target:
-                left = left+1
-            
+        while i<j:
+            if (numbers[i]+numbers[j]) == target:
+                return [i+1,j+1]
+            elif (numbers[i]+numbers[j]) > target:
+                j-=1
             else:
-                right = right-1
-
-        return output
-
-
-
-    def twoSum(self, numbers, target):
-        for i in range(len(numbers)):
-            l, r = i+1, len(numbers)-1
-            tmp = target - numbers[i]
-            while l <= r:
-                mid = l + (r-l)//2
-                if numbers[mid] == tmp:
-                    return [i+1, mid+1]
-                elif numbers[mid] < tmp:
-                    l = mid+1
-                else:
-                    r = mid-1
-
-
-            
+                i+=1
+        return []
