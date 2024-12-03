0dc97469227122f76c6540c7ca2bfcb91c0805a9
Vaish <30567157+vaish30@users.noreply.github.com>
2024-12-03 16:06:22 -0500

    Time: 43 ms (6.71%) | Memory: 19.1 MB (6.03%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,28 @@
 class Solution:  
     def trap(self, height: List[int]) -> int:
-        if len(height) <= 2: return 0
+        #at any given point max water which can be stored is potensial of water - height of the block, where as potential = min(max_left, max_right)
+
+        left_wall = right_wall = 0
         n = len(height)
-        maxLeft, maxRight = height[0], height[n-1]
-        left, right = 1, n - 2
-        ans = 0
-        while left <= right:
-            if maxLeft < maxRight:
-                if height[left] > maxLeft:
-                    maxLeft = height[left]
-                else:
-                    ans += maxLeft - height[left]
-                left += 1
-            else:
-                if height[right] > maxRight:
-                    maxRight = height[right]
-                else:
-                    ans += maxRight - height[right]
-                right -= 1
-        return ans
+        max_left = [0] * n
+        max_right = [0] * n
+
+        for i in range(n):
+            j = -i -1
+            max_left[i] = left_wall
+            max_right[j] = right_wall
+            left_wall = max(left_wall, height[i])
+            right_wall = max(right_wall, height[j])
+        
+        print(max_left)
+        summ = 0
+        for i in range(n):
+            pot = min(max_left[i], max_right[i])
+            summ += max(0, pot - height[i])
+
+
+        return summ
+
+
+        
+       
